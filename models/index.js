4f90bdcd2bd792fc2b05d089f4c30f29e42ffc2a
raddymaster <antellitocci@gmail.com>
2021-07-05 19:10:37 -0400

    add api routes to create, update, delete, and get posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./users');
 const Post = require('./Post');
 
+//create association between users and posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
