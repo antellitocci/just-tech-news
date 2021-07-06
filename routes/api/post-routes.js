f7608a5e8e6476e2a74ca4af5b036c14717dc7c9
raddymaster <antellitocci@gmail.com>
2021-07-05 21:10:51 -0400

    refactor of upvote and upvote functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
-//get all users
+//get all posts
 router.get('/', (req, res) => {
     console.log('=======================');
     Post.findAll({
         //query config
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -18,7 +20,7 @@ router.get('/', (req, res) => {
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
         console.log(err);
-        res.status(500).join(err);
+        res.status(500).json(err);
     });
 });
 
@@ -28,7 +30,7 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         include: [
             {
                 model: User,
@@ -62,6 +64,17 @@ router.post('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
     });
+});
+
+//put upvote a post
+router.put('/upvote', (req, res) => {
+    //custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 
 //update a post's title
@@ -108,5 +121,7 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+
 
 module.exports = router;
