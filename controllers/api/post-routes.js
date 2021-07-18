a6f1d3bd6016a0f404c424cd82cafb7881f02663
raddymaster <antellitocci@gmail.com>
2021-07-17 23:26:41 -0400

    added user upvote and comment functionality on single posts. Also added logic to only show comment form and logout button  if logged in.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -84,13 +84,18 @@ router.post('/', (req, res) => {
 
 //put upvote a post
 router.put('/upvote', (req, res) => {
+    //make sure session exists first
+    if(req.session){
     //custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
         .catch(err => {
             console.log(err);
-            res.status(400).json(err);
+            res.status(500).json(err);
         });
+    }
+
 });
 
 //update a post's title
