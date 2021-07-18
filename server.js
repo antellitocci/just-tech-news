8775566bfb080c958aa91f16ff5e0fde85c34245
raddymaster <antellitocci@gmail.com>
2021-07-17 22:26:36 -0400

    added user login and logout functionality

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SeqeulizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SeqeulizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
